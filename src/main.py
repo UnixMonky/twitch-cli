90a02cea292e58644338d0be06c918c49063de82
Keith Brown <kbrown6@enova.com>
2021-06-02 12:45:23 -0500

    setting twitch-low-latency

--- src/main.py
+++ src/main.py
@@ -108,7 +108,7 @@ def play_url(url, quality=None):
     if quality is None:
         quality = ''
 
-    command = 'streamlink {} {} {}'.format(url, quality, "--twitch-disable-ads --player-passthrough=hls")
+    command = 'streamlink {} {} {}'.format(url, quality, "--twitch-low-latency --twitch-disable-ads")
     process = subprocess.Popen(command.split(), stdout=None, stderr=None)
     output, error = process.communicate()
 
